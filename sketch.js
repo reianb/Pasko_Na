bda30da4fe1ad1786fcf9b94e172fece6d3eb680
Reian Beltran <rmb20j@fsu.edu>
2022-11-18 20:07:17 -0500

    added parol images/illustration

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let red = [];
+
 function setup() {
   createCanvas(400, 400);
 }
 
+function preload(){
+  for(let i = 0; i<2; i++){
+    red[i] = loadImage("assets/red"+[i]+".png");
+  }
+}
+
 function draw() {
   background(125);
+  image(red[0], 0, 0);
+
+}
+
+function hoverStar(){
+  
 }
