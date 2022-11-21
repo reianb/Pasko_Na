e0d631e0a31ed833788c78e6eca7ef0c151380dc
Reian Beltran <rmb20j@fsu.edu>
2022-11-21 13:03:26 -0500

    centered canvas

--- sketch.js
+++ sketch.js
@@ -1,14 +1,28 @@
-let blueOff;
-let blueOn;
+let blue = [];
+let red = [];
+let yellow = [];
+var cnv;
+
+function preload(){
+  for (let i = 0; i < 2; i++) {
+    blue = loadImage("assets/blue"+i+".png");
+    red = loadImage("assets/red"+i+".png");
+    yellow = loadImage("assets/yellow"+i+".png");
+  }
+}
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
 
 function setup() {
-  createCanvas(800, 800);
-  blueOff = createImg("assets/blueOff.png");
-  blueOff.position(-30,-25);
-  
-  blueOff.mouseOver(turnOn);
-  }
+  cnv = createCanvas(1000, 800);
+  background(125);
+  centerCanvas();
+}
 
-function turnOn () {
-  blueOff.hide();
+function draw() {
+
 }
