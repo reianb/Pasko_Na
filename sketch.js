7f86ce0c352e05ad78219464308d32d232c3c242
Reian Beltran <rmb20j@fsu.edu>
2022-11-20 22:51:16 -0500

    mouseOver() and hide() "works"

--- sketch.js
+++ sketch.js
@@ -1,18 +1,8 @@
 let blueOff;
-let blueOn
+let blueOn;
 
 function setup() {
   createCanvas(800, 800);
-}
-
-/**function preload(){
-  for(let i = 0; i<2; i++){
-    red[i] = loadImage("assets/red"+[i]+".png");
-  }
-}**/
-
-function draw() {
-  background(125);
   blueOff = createImg("assets/blueOff.png");
   blueOff.position(-30,-25);
   
