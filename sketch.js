28f605a184292cda57bc8f15b081d2723eadf0f1
Reian Beltran <rmb20j@fsu.edu>
2022-11-21 13:58:31 -0500

    got to position the hovering

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let blue = [];
 let red = [];
 let yellow = [];
 var cnv;
+let blueY = 200;
+let blueX = 300;
 
 function preload(){
   for (let i = 0; i < 2; i++) {
@@ -18,11 +20,14 @@ function centerCanvas() {
 }
 
 function setup() {
-  createCanvas(1000, 800);
-  //centerCanvas();
+  cnv = createCanvas(1000, 800);
+  centerCanvas();
 }
 
 function draw() {
   background(245);
-  image(blue[0],400,400,100,100);
+  if (dist(blueX,blueY,mouseX,mouseY)< 100/2){
+    console.log("you are hovering over the star");
+  }
+  //image(blue[0],400,400,100,100);
 }
