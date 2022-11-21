9c54e40922f1f173ab96d606ca55fa55d8ae40ca
Reian Beltran <rmb20j@fsu.edu>
2022-11-20 22:22:35 -0500

    mouseOver method??

--- sketch.js
+++ sketch.js
@@ -1,21 +1,24 @@
-let red = [];
+let blueOff;
+let blueOn
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
-function preload(){
+/**function preload(){
   for(let i = 0; i<2; i++){
     red[i] = loadImage("assets/red"+[i]+".png");
   }
-}
+}**/
 
 function draw() {
   background(125);
-  image(red[0], 0, 0);
-
-}
-
-function hoverStar(){
+  blueOff = createImg("assets/blueOff.png");
+  blueOff.position(-30,-25);
   
+  blueOff.mouseOver(turnOn);
+  }
+
+function turnOn () {
+  blueOff.hide();
 }
