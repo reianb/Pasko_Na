9dbcd6145bdcd94cba92d65d96ad753c7d9c1b32
Reian Beltran <rmb20j@fsu.edu>
2022-11-27 23:36:56 -0500

    countdown??

--- sketch.js
+++ sketch.js
@@ -1,19 +1,24 @@
 let blue = [];
 let red = [];
 let yellow = [];
+let time =[];
 var cnv;
-let blueY = 350;
+let blueY = 370;
 let blueX = 200;
-let redY = 350;
+let redY = 370;
 let redX = 550;
 let yellowX = 900;
-let yellowY = 350;
+let yellowY = 370;
+let count = 0;
 
 function preload(){
   for (let i = 0; i < 2; i++) {
     blue[i] = loadImage("assets/blue"+i+".png");
     red[i] = loadImage("assets/red"+i+".png");
     yellow[i] = loadImage("assets/yellow"+i+".png");
+  }
+  for (let i =0; i < 1; i++) {
+    time[i] = loadImage("assets/time"+i+".png");
   }
 }
 
@@ -30,7 +35,9 @@ function setup() {
 }
 
 function draw() {
-  background(245);
+  setInterval(countdownBg, 1000);
+  background(125,164,120);
+  image(time[count],550,400);
   image(blue[0],blueX,blueY,300,300);
   image(red[0],redX,redY,300,300);
   image(yellow[0],yellowX,yellowY,300,300);
@@ -46,5 +53,13 @@ function draw() {
     image(yellow[1],yellowX,yellowY,300,300);
   }
 
+}
+
+function countdownBg(){
+  count ++;
+
+    if (count >= 2){
+      count = 0;
+    }
 }
   
